0bc0bae5e8de3348f217773635861e00d5c0608f
SlackyStudent <62130231+SlackyStudent@users.noreply.github.com>
2020-05-17 19:30:32 +1000

    add env

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from google.cloud import firestore
 
-
 # configuration
 DEBUG = True
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
-db = firestore.Client()
 
+# Firestore init
+# instantiate client
+db = firestore.Client()
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
