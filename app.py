b0df264895ddc72ced55e0632fccd6f14bac8119
SlackyStudent <62130231+SlackyStudent@users.noreply.github.com>
2020-05-19 02:38:42 +1000

    Create group chatRoom

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google.cloud import firestore
 
@@ -11,16 +11,22 @@ app.config.from_object(__name__)
 
 # Firestore init
 # instantiate client
-db = firestore.Client()
+db = firestore.Client.from_service_account_json('cc-steam-chat-47d8cdf25925.json')
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-# sample route
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return jsonify('hi world!')
+# Main chatRoom route
+@app.route('/chatRoom', methods=['POST'])
+def add_chatRoom():
+    appId = request.form.get("appId")
+    doc_ref = db.collection('chatRooms').document(appId)
+    doc_ref.set({
+        'Game Name': request.form.get("gameName")
+    })
+
+
 
 
 if __name__ == '__main__':
