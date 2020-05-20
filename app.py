47ff9709e3c16ee3131906e0cb12d517bdce057a
SlackyStudent <62130231+SlackyStudent@users.noreply.github.com>
2020-05-20 18:14:04 +1000

    add messages to db

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google.cloud import firestore
+import time
 
 # configuration
 DEBUG = True
 
 # instantiate the app
 app = Flask(__name__)
+
 app.config.from_object(__name__)
 
 # Firestore init
@@ -14,20 +16,40 @@ app.config.from_object(__name__)
 db = firestore.Client.from_service_account_json('cc-steam-chat-47d8cdf25925.json')
 
 # enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app)
+cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 # Main chatRoom route
-@app.route('/chatRoom', methods=['POST'])
+@app.route('/add_chatRoom', methods=['POST'])
 def add_chatRoom():
     appId = request.form.get("appId")
     doc_ref = db.collection('chatRooms').document(appId)
     doc_ref.set({
         'Game Name': request.form.get("gameName")
     })
+    return jsonify({'test': 'no change'})
 
 
-
+# add message to database
+@app.route('/sendMessage', methods=['POST'])
+def sendMessage():
+    appId = request.form.get("appId")
+    steamId = request.form.get("steamId")
+    message = request.form.get("message")
+    room_ref = db.collection('chatRooms').document(appId)
+    ts = str(int(time.time()))
+    message_ref = room_ref.collection('messages').document(ts)
+    message_ref.set({
+        'SteamId': steamId,
+        'message': message,
+        'timestamp': ts,
+        'displayTime': firestore.SERVER_TIMESTAMP
+    })
+    return jsonify({'test': 'no change'})
 
 if __name__ == '__main__':
     app.run()
+
+
+
