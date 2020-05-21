4300d8530854da965392572bd7da7c56e9aeb408
SlackyStudent <62130231+SlackyStudent@users.noreply.github.com>
2020-05-22 00:19:19 +1000

    add realtime chat responses

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google.cloud import firestore
+import threading
 import time
 
 # configuration
@@ -18,8 +19,8 @@ db = firestore.Client.from_service_account_json('cc-steam-chat-47d8cdf25925.json
 # enable CORS
 CORS(app)
 cors = CORS(app, resources={r'/*': {'origins': '*'}})
-
-
+allDocs = {}
+updateChat = threading.Event()
 # Main chatRoom route
 @app.route('/add_chatRoom', methods=['POST'])
 def add_chatRoom():
@@ -28,7 +29,54 @@ def add_chatRoom():
     doc_ref.set({
         'Game Name': request.form.get("gameName")
     })
-    return jsonify({'test': 'no change'})
+    room_ref = db.collection('chatRooms').document(appId)
+    # # # Get all current message from firestore and sent to front
+    # doc_ref = room_ref.collection('messages').order_by('timestamp', direction=firestore.Query.ASCENDING).stream()
+    #
+    #
+    # allDocs = [doc.to_dict() for doc in doc_ref]
+    # print("Print one by one:")
+    # for doc in allDocs:
+    #     print(doc)
+    # return jsonify(allDocs), 200
+
+    # [START listen_for_changes]
+    # Create an Event for notifying main thread.
+    query_done = threading.Event()
+    # Create a callback on_snapshot function to capture changes
+    global allDocs
+    global updateChat
+    def on_snapshot(col_snapshot, changes, read_time):
+        print(u'Callback received query snapshot.')
+        print(u'Current messages ')
+
+        global allDocs
+        global updateChat
+        allDocs = [change.document.to_dict() for change in changes if change.type.name == 'ADDED']
+        for change in allDocs:
+            print(change)
+            print("message printed")
+        query_done.set()
+        updateChat.set()
+        print("After set")
+    col_query = room_ref.collection('messages')
+    # Watch the collection query
+    query_watch = col_query.on_snapshot(on_snapshot)
+    # Wait for the callback captures the deletion.
+    print("before timout")
+    query_done.wait(timeout=60)
+    print("After timeout")
+    print("after unsub")
+    updateChat.clear()
+    return jsonify(allDocs)
+# realtime updates to the chat
+@app.route('/updateChat', methods=['POST'])
+def update_chat():
+    global allDocs
+    global updateChat
+    updateChat.wait()
+    updateChat.clear()
+    return jsonify(allDocs)
 
 
 # add message to database
@@ -41,15 +89,13 @@ def sendMessage():
     ts = str(int(time.time()))
     message_ref = room_ref.collection('messages').document(ts)
     message_ref.set({
-        'SteamId': steamId,
+        'steamId': steamId,
         'message': message,
         'timestamp': ts,
         'displayTime': firestore.SERVER_TIMESTAMP
     })
     return jsonify({'test': 'no change'})
 
+
 if __name__ == '__main__':
     app.run()
-
-
-
